efab5071d58ffdbebb80404af9cb06cb90a1a6f4
RobertoNittolo <nitrobert@yahoo.com>
2020-08-08 15:49:04 -0400

    Added request handler class

---
+++ Assets/Assets/Scripts/Helpers/RequestHandler.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using UnityEngine;
+using UnityEngine.Networking;
+
+public class RequestHandler : MonoBehaviour
+{
+    IEnumerator GetRequest(string uri)
+    {
+        UnityWebRequest unityWebRequest = UnityWebRequest.Get(uri);
+        yield return unityWebRequest.SendWebRequest();
+
+        if (unityWebRequest.isNetworkError)
+        {
+            Debug.Log("Error While Sending: " + unityWebRequest.error);
+        }
+        else
+        {
+            Debug.Log("Received: " + unityWebRequest.downloadHandler.text);
+        }
+    }
+}
