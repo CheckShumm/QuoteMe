5c38303a5ebb5d21d60567605776bb9d438fe786
DMediati <davidmediati@gmail.com>
2020-08-08 04:12:16 -0400

    Added singleton protection

--- Assets/Assets/Scripts/Systems/ServiceManager.cs
+++ Assets/Assets/Scripts/Systems/ServiceManager.cs
@@ -1,5 +1,27 @@
-﻿public static class ServiceManager
+﻿using UnityEngine;
+
+public static class ServiceManager
 {
-    public static AppManager AppManager;
-    public static ViewManager ViewManager;
+    public static AppManager AppManager { get {return _appManager.Value;} set { _appManager.Value = value; } }
+    public static ViewManager ViewManager { get {return _viewManager.Value;} set { _viewManager.Value = value; } }
+
+
+    // Singleton protection
+    private static readonly WriteOnce<AppManager> _appManager = new WriteOnce<AppManager>();
+    private static readonly WriteOnce<ViewManager> _viewManager = new WriteOnce<ViewManager>();
+}
+
+public sealed class WriteOnce<T> 
+{
+    private T _value;
+    private bool _hasValue;
+    public T Value {
+        get { return _value; }
+        set 
+        {
+            Debug.Assert(!_hasValue, "Trying to set a Singleton more than once!");
+            _value = value;
+            _hasValue = true;
+        }
+    } 
 }
