27c62bf75bd952110401a297c432ccbbdec033d4
David Mediati <davidmediati@gmail.com>
2020-08-08 01:24:06 -0400

    Unity Project Setup

---
+++ Assets/Assets/Scripts/Helpers/AutoScale.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+[RequireComponent(typeof(CanvasScaler))]
+public class AutoScale : MonoBehaviour
+{
+    [SerializeField] private CanvasScaler _scaler;
+    private void Reset() 
+    {
+        _scaler = gameObject.GetComponent<CanvasScaler>();
+    }
+
+    private void Start()
+    {
+        SetScalerDirection();
+    }
+
+#if UNITY_EDITOR
+    // Update is called once per frame
+    private void Update()
+    {
+        SetScalerDirection();
+    }
+#endif
+
+    void SetScalerDirection()
+    {   
+        float widthRatio = _scaler.referenceResolution.x / Screen.width;
+        float heightRatio = _scaler.referenceResolution.y / Screen.height;
+        _scaler.matchWidthOrHeight = widthRatio > heightRatio ? 0 : 1;
+    }
+}
