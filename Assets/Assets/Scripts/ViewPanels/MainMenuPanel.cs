d72539332a7a25d39f7704a07fe8317c6d029954
checkshumm <checkshumm@gmail.com>
2020-08-08 17:04:09 -0400

    Added more panels

--- Assets/Assets/Scripts/ViewPanels/MainMenuPanel.cs
+++ Assets/Assets/Scripts/ViewPanels/MainMenuPanel.cs
@@ -1,16 +1,22 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using Photon.Pun;
 
 public class MainMenuPanel : BasePanel
 {
-
-   public void ExtCreateRoom() {
-        ServiceManager.ViewManager.TransitToRoom();
-   }
 
    public void ExtPlay()
    {
        ServiceManager.ViewManager.TransitToGameplay();
    }
+
+    // Create a room
+    private void ExtCreateRoom(string roomName)
+    {
+        Debug.Log("created room " + roomName);
+        PhotonNetwork.CreateRoom(roomName);
+        ServiceManager.ViewManager.TransitToRoom();
+    }
+
 }
