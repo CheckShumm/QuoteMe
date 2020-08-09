3d10f3c39865e6a5ed9274847d1203b13482cd8b
DMediati <davidmediati@gmail.com>
2020-08-09 04:20:27 -0400

    Changed Photon Room handling

--- Assets/Assets/Scripts/Systems/AppManager.cs
+++ Assets/Assets/Scripts/Systems/AppManager.cs
@@ -5,28 +5,13 @@ using Photon.Pun;
 
 public class AppManager : BaseManager
 {
-    private const string gameVersion = "0.0.1"; 
+    public const string GameVersion = "1.0.0"; 
 
     protected override void RegisterManager() { ServiceManager.AppManager = this; }
 
     private void Start() 
     {
-        // connect to photon
-        Connect();
-
         PlayerManager.Initialize();
-        RoomManager.Initialize();
-        DontDestroyOnLoad(new GameObject("ChatManager", typeof(ChatManager)));
-    }
-
-    // connects user to photon network
-    private void Connect()
-    {
-        if (!PhotonNetwork.IsConnected)
-        {
-            Debug.Log("Connecting to photon network");
-            PhotonNetwork.ConnectUsingSettings();
-            PhotonNetwork.GameVersion = gameVersion;
-        }
+        RoomManager.Initialize();        
     }
 }
