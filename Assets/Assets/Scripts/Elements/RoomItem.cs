3da2c481dec6e1a415fa97096fe1954cd3e798b6
DMediati <davidmediati@gmail.com>
2020-08-09 02:36:06 -0400

    Updated RoomList view, modified photon network connection logic

--- Assets/Assets/Scripts/Elements/RoomItem.cs
+++ Assets/Assets/Scripts/Elements/RoomItem.cs
@@ -9,16 +9,29 @@ public class RoomItem : MonoBehaviour
     [SerializeField] private TextMeshProUGUI _roomName = null;
     [SerializeField] private TextMeshProUGUI _playerCount = null;
     [SerializeField] private TextMeshProUGUI _hostName = null;
+    [SerializeField] private Transform _border;
 
-
-
-    public void Initialize(string roomName, int playerCount, string hostName)
+    public void Initialize(string roomName, int playerCount, int maxPlayers, string hostName)
     {
         gameObject.SetActive(true);
         _roomName.SetText(roomName);
-        _playerCount.SetText(playerCount.ToString());
+        _playerCount.SetText(playerCount.ToString() + "/" + maxPlayers.ToString());
         _hostName.SetText(hostName);
-    
+        int rand = Random.Range(0, 3);
+        switch(rand) 
+        {
+            case 0:
+                _border.Rotate(180,0,0);
+                break;
+            case 1:
+                _border.Rotate(0,180,0);
+                break;
+            case 2:
+                _border.Rotate(0,0,180);
+                break;
+            default:
+                break;
+        }
     }
    
 }
