2109e95e1c88d010ae4f9fe244f3f2be180a4056
checkshumm <checkshumm@gmail.com>
2020-08-09 14:25:02 -0400

    Missing quotes files

---
+++ Assets/Assets/Scripts/Elements/AuthorImage.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class AuthorImage : MonoBehaviour
+{
+    [SerializeField] private Image _overlay = null;
+
+    public void SetOverlayColor(Color color)
+    {
+        _overlay.color = color;
+    }
+
+}
