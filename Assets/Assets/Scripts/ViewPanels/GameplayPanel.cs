b9b89b22d3e9ea2638d61336628e61b6f712ed58
checkshumm <checkshumm@gmail.com>
2020-08-09 04:22:36 -0400

    Added AuthorImage as element

--- Assets/Assets/Scripts/ViewPanels/GameplayPanel.cs
+++ Assets/Assets/Scripts/ViewPanels/GameplayPanel.cs
@@ -16,29 +16,45 @@ public class GameplayPanel : BasePanel
 
     List<string> fakeQuotes = new List<string>(new string[] { "quote 1", "quote 2", "quote 3", "quote 4", "quote 5" });
 
-    private int correctAuthor = 0;
+    private Color _lightRed = new Color(250,122,122,100);
+    private Color _lightGreen = new Color(250, 122, 122, 100);
+    private Color _white = new Color(255, 255, 255, 255);
+
+    private int _correctAnswer = 0;
 
     // UI elements
     [SerializeField] private Image timer = null;
 
-    [SerializeField] private Image author1 = null;
-    [SerializeField] private Image author2 = null;
-    [SerializeField] private Image author3 = null;
-    [SerializeField] private Image author4 = null;
+    [SerializeField] private Image[] authorImages = new Image[4];
+    private AuthorImage[] _authorImageElements = new AuthorImage[4];
+
     [SerializeField] private TextMeshProUGUI quote = null;
 
+    public void ExtOnClickAuthor(int index)
+    {
+        Debug.Log(index);
+        if (index == _correctAnswer)
+            _authorImageElements[index].SetOverlayColor(_lightGreen);
+        else
+            _authorImageElements[index].SetOverlayColor(_lightRed);
+    }
 
     override protected void OnActivate()
     {
         quote.SetText(fakeQuotes[_currentRound]);
+        _correctAnswer = UnityEngine.Random.Range(0, 3);
     }
 
     private void NextRound()
     {
         _currentRound += 1;
         _timeLeft = _roundTime;
-        quote.SetText(fakeQuotes[_currentRound]);
 
+        foreach (AuthorImage authorImageElement in _authorImageElements)
+            authorImageElement.SetOverlayColor(_white);
+
+        quote.SetText(fakeQuotes[_currentRound]);
+        _correctAnswer = UnityEngine.Random.Range(0, 3);
         if (_currentRound == _totalRounds)
         {
             ServiceManager.ViewManager.TransitToRoom();
