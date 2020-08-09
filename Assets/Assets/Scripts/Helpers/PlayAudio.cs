7d3c29382595b2f80c309990b92a3727925352c5
DMediati <davidmediati@gmail.com>
2020-08-08 23:53:30 -0400

    Added audio system, generic button prefab

---
+++ Assets/Assets/Scripts/Helpers/PlayAudio.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayAudio : MonoBehaviour
+{
+    [SerializeField] private AudioClip _clip;
+    public void ExtPlayOneShot() 
+    {
+        ServiceManager.ViewManager.SfxAudioSource.PlayOneShot(_clip, 1.0f);
+    }
+}
