0bf21bd20cfb1436196cc0ccb232659eeeae95ab
DMediati <davidmediati@gmail.com>
2020-08-08 22:02:40 -0400

    Added sprites and main menu polish

---
+++ Assets/Assets/Scripts/Helpers/ObjectWiggle.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ObjectWiggle : MonoBehaviour
+{
+    [SerializeField] private float _wiggleDelta;
+    [SerializeField] private float _wiggleTime;
+
+    private void Start() 
+    {
+        transform.Rotate(0, 0, -_wiggleDelta);
+        LeanTween.rotateZ(gameObject, transform.localEulerAngles.z + 2 * _wiggleDelta, _wiggleTime).setLoopPingPong();
+    }
+}
